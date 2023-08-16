90d5607026c6754a554115e98ccc5fca374fca19
YHDF <unes.houdaifa@gmail.com>
2023-08-16 16:41:32 +0200

    FEAT(Portfolio-Client)(WIP): Ajout d'un nouvel component angular 'Me' + Compression/Implementation du loader 'Draco' pour un temps de chargement plus rapide + Abstraction du code du mouvement de la caméra et Externalisation des paramètres

---
+++ portfolio-client/src/app/shared/services/animation-config.service.ts
@@ -0,0 +1,43 @@
+import {Inject, Injectable} from '@angular/core';
+import * as THREE from 'three';
+import {AnimationConfig} from "../../three-model/three-model";
+import {ANIMATION_CONFIG_TOKEN} from "../../three-model/animation-config.token";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimationConfigService {
+
+  constructor(@Inject(ANIMATION_CONFIG_TOKEN) private readonly animationConfig: AnimationConfig) {
+  }
+
+  animateCamera(gsap : any, camera: THREE.Camera, animationId : number, __callback : (target : THREE.Vector3) => void, __toggleCallback? : () => void) {
+    // GSAP animation for moving the camera
+    const config = this.animationConfig!.animations.filter((animation : any) => animation['animationId'] === animationId )[0];
+    const path = config.path.map((coords: number[]) => new THREE.Vector3(...coords));
+    const curve = new THREE.CubicBezierCurve3(...path);
+    const points = curve.getPoints(config.numPoints);
+    const target = new THREE.Vector3(...config.target);
+    const delay = config.delay;
+    __callback.apply(null,[target]);
+    const animationOnComplte = new Promise(resolve => {
+       let tl = gsap.timeline({ repeat: config.repeat, delay: delay ? delay : 0, onComplete : resolve })
+      for (let i = 0; i < points.length; i++) {
+        let pt = points[i];
+        tl.to(camera.position, {
+          duration: config.duration,
+          x: pt.x,
+          y: pt.y,
+          z: pt.z,
+          ease: config.ease,
+          onUpdate: () => {
+            camera.lookAt(target);
+          }
+        });
+      }
+    });
+
+    animationOnComplte.then(value => __toggleCallback?.apply(null));
+  }
+}
---
+++ portfolio-client/src/app/three-model/animation-config.token.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from '@angular/core';
+import { AnimationConfig } from './three-model';
+
+export const ANIMATION_CONFIG_TOKEN = new InjectionToken<AnimationConfig>('animationConfig');
