46f4d16772dc5930291f52dfb416c78c709d3714
YHDF <unes.houdaifa@gmail.com>
2023-08-20 16:03:46 +0200

    FEAT(Portfolio-Client)(WIP): Ajout d'un nouvel component angular 'Me' + Compression/Implementation du loader 'Draco' pour un temps de chargement plus rapide + Abstraction du code du mouvement de la caméra et Externalisation des paramètres

--- portfolio-client/src/app/three-model/three-model.component.ts
+++ portfolio-client/src/app/three-model/three-model.component.ts
@@ -24,7 +24,7 @@ gsap.registerPlugin(MotionPathPlugin);
 })
 export class ThreeModelComponent implements AfterViewInit {
 
-  showMe = true;
+  showMe = false;
 
   constructor(private animationConfigService: AnimationConfigService,
               private el: ElementRef,
